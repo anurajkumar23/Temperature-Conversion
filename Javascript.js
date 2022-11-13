80b10f51c807cec566355d149a811b6626eff2cc
Anuraj Kumar <anurajkumar6294@gmail.com>
2022-11-13 13:48:13 +0530

    change files

---
+++ Javascript.js
@@ -0,0 +1,28 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    
+    const tempSelected = document.getElementById('temp_diff');
+    
+    const valueTemp = tempSelected.options[tempSelected.selectedIndex].value;
+    
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round(cel * 9/5 + 32);
+        return fahrenheit;
+    }
+    
+    const fahToCel = (fah) => {
+        let celcius = Math.round((fah - 32) * 5/9);
+        return celcius;
+    }
+    
+    let result;
+    
+    if(valueTemp == 'cel') {
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Fahrenheit`;
+    }
+        else {
+            result = fahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Celcius`;
+        }
+    }
